66cacac8e9a3fb5f4c77113c8965d3409ce1ac3a
KYJ <nuyoo387@gmail.com>
2025-06-25 11:30:59 +0900

    react project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base : '/', 
+  plugins: [react()],
+  // server의 proxy 설정
+  // 5173 port -> 8088 port
+  // 포트번호가 다를 경우 Cross-origin으로 간주하고 차단(CORS)
+  server : {
+    proxy : {
+      '/api' : 'http://localhost:8088'
+    }
+  }
+})
